de0094d5c25b46fbf75b86ac3820e590b4e2b5f8
James Petersen <m@jamespetersen.ca>
2026-02-21 19:26:58 -0400

    Fix Stupid Syntax Mistake

--- data_gen_templates/rules.py
+++ data_gen_templates/rules.py
@@ -108,6 +108,7 @@ class Rules:
         reqd_items = [f"mon_{pevo.species}"]
         if mthd.startswith("trade"):
             reqd_items.append(items.items["linking_cord"].label)
+            reqd_items.append(items.items["bag"].label)
         if mthd.endswith("day"):
             reqd_items.append(items.items["daytime"].label)
             reqd_items.append(items.items["poketch"].label)
@@ -116,7 +117,7 @@ class Rules:
             reqd_items.append(items.items["poketch"].label)
         if pevo.item is not None:
             reqd_items.append(pevo.item)
-            if not "held" in mthd:
+            if "held" not in mthd:
                 reqd_items.append(items.items["bag"].label)
             if (pevo.item not in items.reusable_evo_items or pevo.item.startswith("TM") and not options.reusable_tms) and not options.evo_items_shop_in_ap_helper:
                 reqd_items.append("event_veilstone_store")
